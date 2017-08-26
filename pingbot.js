0f223e5ecde1fc142acb257bd142d59393f42781
ehwuts matsu <tsukan.git@gmail.com>
2017-08-26 00:14:46 -0700

    trigger enhancement

--- pingbot.js
+++ pingbot.js
@@ -10,8 +10,11 @@ discord.on("ready", () => {
 
 discord.on("messageCreate", (msg) => {
 	if (msg.channel.id === config.discord_channel && msg.author.id != discord.user.id) {
-		if (msg.content.indexOf("ping") !== -1) {
+		if (msg.content.toLowerCase.indexOf("ping") !== -1) {
 			discord.createMessage(msg.channel.id, "Ping!");
+		}
+		if (msg.content.toLowerCase.indexOf("pong") !== -1) {
+			discord.createMessage(msg.channel.id, "Ping..");
 		}
 	}
 });
