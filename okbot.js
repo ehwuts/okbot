9a28afc652b54ce2658c03091cb4d6b7ee18db28
ehwuts matsu <tsukan.git@gmail.com>
2018-08-19 19:45:55 -0700

    fix commands command

--- okbot.js
+++ okbot.js
@@ -8,9 +8,12 @@ var discord = new Eris(config.discord_token);
 var regex_strip_module = /[\/\\\.]/;
 
 function loadModule(module) {
+	let pathModule = './bot_modules/' + module.replace(regex_strip_module, '');
+	//console.log('Loading module ' + pathModule);
 	try {
-		return require('./bot_modules/' + module.replace(regex_strip_module, ""));
+		return require(pathModule);
 	} catch (e) {
+		console.log(e);
 		return false;
 	}
 }
@@ -26,7 +29,7 @@ for (let prop in config.discord_bindings) {
 				if (mm) {
 					modules[m] = mm;
 				} else {
-					console.log("Warning: Unknown module \"" + m + "\" requested by " + prop + ".");
+					console.log("Warning: Unknown module \"" + m.replace(regex_strip_module, '') + "\" requested by " + prop + ".");
 				}
 			}
 		}
@@ -38,6 +41,7 @@ for (let prop in modules) {
 			if (command_handlers[modules[prop].commands[i]] !== undefined) {
 				console.log("Warning: multiple modules want command -- " + modules[prop].commands[i]);
 			}
+			//console.log('Registering command ' + modules[prop].commands[i] + ' for module ' + prop);
 			command_handlers[modules[prop].commands[i]] = prop;
 		}
 	}
@@ -50,7 +54,7 @@ var responseFunc = (id, txt) => {
 /* End Module Registration */
 
 /* Discord Event Handler Registry */
-var regex_cmd = /^!([a-z]+)(?: |$)/i;
+var regex_cmd = /^!([\w\d]+)(?: |$)/i;
 
 discord.on("ready", () => {
 	console.log(":okbot online.");
@@ -60,7 +64,24 @@ discord.on("messageCreate", (msg) => {
 	if (msg.author.id != discord.user.id && config.discord_bindings[msg.channel.id] !== undefined) {
 		var cmd = msg.content.toLowerCase().match(regex_cmd);
 		if (cmd !== null) {
-			if (command_handlers[cmd[1]] !== undefined && config.discord_bindings[msg.channel.id].modules.indexOf(command_handlers[cmd[1]]) !== -1) {
+			if (cmd[1] == 'commands' || cmd[1] == 'modules') {
+				let list = [];
+				let outstr = '';
+				for (let i = 0; i < config.discord_bindings[msg.channel.id].modules.length; i++) {
+					//console.log('module ' + config.discord_bindings[msg.channel.id].modules[i]);
+					for (let j = 0; j < modules[config.discord_bindings[msg.channel.id].modules[i]].commands.length; j++) {
+						//console.log('command ' + modules[config.discord_bindings[msg.channel.id].modules[i]].commands[j]);
+						list.push(modules[config.discord_bindings[msg.channel.id].modules[i]].commands[j]);
+					}
+					outstr += config.discord_bindings[msg.channel.id].modules[i] + ' (' + list.join(', ') + '), ';
+					list = [];
+				}
+				if (outstr == '') {
+					responseFunc(msg.channel.id, 'There are no modules active on this channel.');
+				} else {
+					responseFunc(msg.channel.id, 'The modules active on this channel are: ' + outstr.substr(0, outstr.length - 2));
+				}
+			} else if (command_handlers[cmd[1]] !== undefined && config.discord_bindings[msg.channel.id].modules.indexOf(command_handlers[cmd[1]]) !== -1) {
 				console.log("Performing \""+command_handlers[cmd[1]]+":"+ cmd[1] +"\" on \"" + msg.channel.name + ":" + msg.channel.id + "\".");
 				modules[command_handlers[cmd[1]]].respond(cmd, msg, responseFunc);
 			} else {
